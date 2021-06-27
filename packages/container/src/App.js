007690f92496014a41dbe3f74fc27cc162cd63f9
Hugo Andrés Díaz Bernal <Hugo.Diaz@bitsamericas.com>
2021-06-26 20:49:04 -0500

    Initial project, MFE

---
+++ packages/container/src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import MarketingApp from './components/MarketingApp';
+
+const App = () => {
+	return (
+		<Fragment>
+			<h1>Hola Mundo de los microfronts</h1>
+			<hr />
+			<MarketingApp />
+		</Fragment>
+	);
+};
+
+export default App;
